1ec2d833c3ccc82b47bcb81192b50013c2088497
adeelkhan18 <FA16-BSE-135@ISBSTUDENT.COMSATS.EDU.PK>
2020-06-02 09:42:31 +0500

    First commit

---
+++ resources/views/administrator/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+    </div>
+    <strong>{{ __('Copyright') }} &copy; 2019 <a href="#" target="_blank">{{ __('Shafiq Raes') }}</a>.</strong> {{ __('All rights reserved.') }}
+</footer>
---
+++ storage/framework/views/038f2d47119ab0adaa88f2ba0f8d7469491de4b2.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+    </div>
+    <strong><?php echo __('Copyright'); ?> &copy; 2019 <a href="#" target="_blank"><?php echo __('Shafiq Raes'); ?></a>.</strong> <?php echo __('All rights reserved.'); ?>
+
+</footer>
